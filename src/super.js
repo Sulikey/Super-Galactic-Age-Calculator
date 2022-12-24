dce1b70a6cfae6256f2d0d95ff02ab7262136be6
sulikey <anahi.sulikey@gmail.com>
2022-12-23 21:07:32 -0800

    add refactor added keys to construtor passed

--- src/super.js
+++ src/super.js
@@ -1,22 +1,31 @@
 export default class Age {
   constructor(earthAge) {
     this.earthAge = earthAge;
+    this.mercuryAge = 0;
+    this.venusAge = 0;
+    this.marsAge = 0;
+    this.jupiterAge = 0;
+    
   }
 
   mercury() {
     this.earthAge = parseInt((this.earthAge / 0.24).toFixed());
+    this.mercuryAge = parseInt((this.earthAge / 0.24).toFixed());
   }
 
   venus() {
     this.earthAge = parseInt((this.earthAge / 0.62).toFixed());
+    this.venusAge = parseInt((this.earthAge / 0.62).toFixed());
   }
 
   mars() {
     this.earthAge = parseInt((this.earthAge / 1.88 ).toFixed());
+    this.marsAge = parseInt((this.earthAge / 1.88 ).toFixed());
   }
 
   jupiter() {
     this.earthAge = parseInt((this.earthAge / 11.86).toFixed());
+    this.jupiterAge = parseInt((this.earthAge / 11.86).toFixed());
   }
-  
+
 }
