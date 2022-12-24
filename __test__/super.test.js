a444248a3c4e5df41750f4d72502182142447ea1
sulikey <anahi.sulikey@gmail.com>
2022-12-23 18:50:55 -0800

    add red phase

---
+++ __test__/super.test.js
@@ -0,0 +1,11 @@
+import Age from "../src/super.js";
+
+describe("Age", () => {
+  let planetAge;
+  beforeEach ( ()=> {
+    planetAge = new Age(32);
+  });
+
+  test("should show an Age constructor with properties", () => {
+    expect(planetAge.earthAge).toEqual(32);
+  });
