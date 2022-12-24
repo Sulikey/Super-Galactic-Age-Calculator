1ee989c53929d4414c81f2f74ecb89342b5c4b59
sulikey <anahi.sulikey@gmail.com>
2022-12-23 20:02:39 -0800

    add green phase venus passed

--- src/super.js
+++ src/super.js
@@ -2,7 +2,12 @@ export default class Age {
   constructor(earthAge) {
     this.earthAge = earthAge;
   }
+
   mercury() {
     this.earthAge = parseInt((this.earthAge / 0.24).toFixed());
+  }
+
+  venus() {
+    this.earthAge = parseInt((this.earthAge / 0.62).toFixed());
   }
 }
